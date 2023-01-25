e948e329f9eff77ff1ab6842b1f4b9d6c6cda252
Roma Joy <joynormahouser@gmail.com>
2023-01-25 10:35:10 -0600

    opening day test

---
+++ loops.js
@@ -0,0 +1,20 @@
+// 1. Create a loop that logs the numbers from 0-99 (ascending)
+for (let i=0; i < 100; i++){
+    console.log(i);
+}
+// 2. Create a loop that logs the numbers from 99-0 (descending)
+for (let i=99; i>=0; i--){
+    console.log(i);
+}
+// 7. Create a loop that logs the numbers from 49-72 (ascending)
+for (let i=49; i <= 79; i++){
+    console.log(i);
+}
+// 8. Create a loop that logs the unmbers from 81-26 (descending)
+for (let i=81; i >=26; i--) {
+    console.log(i);
+}
+// 9. Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+for (let i=3; i < 91; i+=3){
+    console.log(i);
+}
